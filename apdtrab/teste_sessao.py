4f250ccf4ef0b3eb99f36d66188e280891eee76c
GuilhermeCarrato <piocarrato1234@gmail.com>
2017-05-12 04:50:50 -0300

    talvez tenha dado certo
    troquei esse valores achando que assim ele esperario o resultado da
    lista dentra da lista recebida

--- apdtrab/teste_sessao.py
+++ apdtrab/teste_sessao.py
@@ -6,11 +6,11 @@ class teste_sessao (unittest.TestCase):
         controle_sessao.remover_todas_sessoes()
     def teste_buscar_sessao(self):
         controle_sessao.criar_sessao(1,"Alien",1300,12)
-        t = controle_sessao.buscar_sessao(1)
-        self.assertEqual(1,t[0])
-        self.assertEqual("Alien", t[1])
-        self.assertEqual(1300,t[2])
-        self.assertEqual(12, t[3])
+        t = controle_sessao.listar_sessao(1)
+        self.assertEqual(1,t[0][0])
+        self.assertEqual("Alien", t[0][1])
+        self.assertEqual(1300,t[0][2])
+        self.assertEqual(12, t[0][3])
 
 if __name__ == '__main__':
     unittest.main(exit=False)
